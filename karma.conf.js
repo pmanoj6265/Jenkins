d7e2c3bbbe5d6a3b718abb862f2e4aab4060b33d
Manoj <pmanoj6265@gmail.com>
2017-07-27 16:33:13 -0400

    karma file added

---
+++ karma.conf.js
@@ -0,0 +1,10 @@
+reporters: ['progress', 'coverage', 'dots', 'junit'],
+
+    junitReporter: {
+    outputDir: 'karma-results',
+        outputFile: 'karma-results.xml'
+},
+
+browsers: ['PhantomJS'],
+
+    singleRun: true
